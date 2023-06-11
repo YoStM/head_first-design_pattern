fc0317e2478e89ec4bf1a1a83611177622606148
YoStM <yo.stm@outlook.com>
2023-06-11 14:21:08 +0200

    init project with tests

---
+++ src/main/java/fr/saintmartin/yohan/headfirst/DesignPatterns.java
@@ -0,0 +1,7 @@
+package fr.saintmartin.yohan.headfirst;
+
+public class DesignPatterns {
+    public static void main(String[] args) {
+        System.out.println("Welcome to HeadFirst Design patterns !");
+    }
+}
---
+++ src/test/java/fr/saintmartin/yohan/headfirst/DesignPatternsTests.java
@@ -0,0 +1,11 @@
+package fr.saintmartin.yohan.headfirst;
+
+import org.junit.jupiter.api.Test;
+
+public class DesignPatternsTests {
+
+    @Test
+    public void nothing() {
+    }
+
+}
